588bc7ecbea38c2034832456fc28143e63b83e24
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2024-10-08 20:24:09 -0300

    tela de ação

---
+++ src/br/com/cesarschool/poo/telas/telasacao/TelaAlterarAcao.java
@@ -0,0 +1,73 @@
+package br.com.cesarschool.poo.telas.telasacao;
+
+import br.com.cesarschool.poo.titulos.entidades.Acao;
+import br.com.cesarschool.poo.titulos.mediators.MediatorAcao;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class TelaAlterarAcao {
+    private JPanel panel;
+    private JPanel panel1;
+    private JTextField idField1;
+    private JTextField nomeField1;
+    private JTextField dataField1;
+    private JTextField valorField2;
+    private JButton enviarButton;
+    private JButton buscarButton;
+    private JTextPane textoTextPane;
+
+    public TelaAlterarAcao() {
+        buscarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MediatorAcao acao = new MediatorAcao();
+                int id = Integer.parseInt(idField1.getText());
+                try {
+                    Acao acao1 = acao.buscar(id);
+
+                    if(acao1 != null) {
+                        nomeField1.setText(acao1.getNome());
+                        dataField1.setText(acao1.getDataValidade().toString());
+                        valorField2.setText(String.valueOf(acao1.getValorUnitario()));
+                    }
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        enviarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MediatorAcao acao = new MediatorAcao();
+                int id = Integer.parseInt(idField1.getText());
+                String nome = nomeField1.getText();
+                LocalDate data = LocalDate.parse(dataField1.getText());
+                double valor = Double.parseDouble(valorField2.getText());
+
+                Acao novaAcao = new Acao(id, nome, data, valor);
+
+                try {
+                    String message = acao.alterar(novaAcao);
+                    if(message != null) {
+                        JOptionPane.showMessageDialog(null, message);
+                    } else {
+                        message = "Ação alterada com sucesso!";
+                        JOptionPane.showMessageDialog(null, message);
+                    }
+                    JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(enviarButton);
+                    frame.dispose();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+    }
+
+    public JPanel getPanel1() {
+        return panel;
+    }
+}
--- src/br/com/cesarschool/poo/titulos/entidades/Acao.java
+++ src/br/com/cesarschool/poo/titulos/entidades/Acao.java
@@ -7,11 +7,11 @@ import java.time.LocalDate;
  * E deve ter os seguintes atributos:
  * valorUnitario, do tipo double.
  * 
- * Deve ter um construtor público que inicializa os atributos, 
- * e métodos set/get públicos para os atributos.
+ * Deve ter um construtor peblico que inicializa os atributos,
+ * e metodos set/get peblicos para os atributos.
  * 
- * Deve ter um metodo público double calcularPrecoTransacao(double montante): o preço
- * da transação é o produto do montante pelo valorUnitario.
+ * Deve ter um metodo publico double calcularPrecoTransacao(double montante): o preÃ§o
+ * da transaÃ§Ã£o Ã© o produto do montante pelo valorUnitario.
  */
 public class Acao extends Ativo {
     double valorUnitario;
